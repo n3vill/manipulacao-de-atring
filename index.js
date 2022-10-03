363112c3b6c8405dc68fd03d8434fd04a96e3af9
n3vill <neville_vieira@outlook.com>
2022-10-03 08:43:01 -0300

    treinando manipulação de strings

---
+++ index.js
@@ -0,0 +1,20 @@
+// let te = "0123456789";
+/* let texto = "Mergulhando nas profundezas";
+let texto = "Sofia21 PCB";
+
+console.log(texto.indexOf("o"));    //retorna 11
+
+console.log (texto.toUpperCase().indexOf("O"))  //MERGULHANDO NAS PROFUNDEZAS - 10
+
+console.log(texto.toUpperCase().substring(3, 9).indexOf("g")) //GULHAN, -1
+
+console.log(texto.split("P"))   //['Sofia', ' PCB']
+
+let chuchu = ["sofia", "21" ][0]
+console.log(array)       //retorna sofia 
+*/
+
+let texto = "Sofia Manzano";  //resultado tem que aparecer Sofia21
+
+console.log (texto.substring(0, 5) + texto.indexOf("f") + texto.indexOf("o"))
+
